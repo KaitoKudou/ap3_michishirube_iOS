e0e7c154e2ccc9e8b8d46cd4e184419efd53c2d3
AnboYukari <b1018129@gmail.com>
2020-09-27 18:05:28 +0900

    [Add]created an evaluation screen

---
+++ michisirube/michisirube/View/NaviEvaluationViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NaviEvaluationViewController.swift
+//  michisirube
+//
+//  Created by 安保友香梨 on 2020/09/27.
+//
+
+import UIKit
+
+class NaviEvaluationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
