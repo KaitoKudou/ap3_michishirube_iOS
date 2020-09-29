989fefb780ccbeb212c4b6bf611e23da25792f03
AnboYukari <b1018129@gmail.com>
2020-09-29 22:12:46 +0900

    [Add] 下線部を表示する関数を作成した

--- michisirube/michisirube/Extension/UIView+Extension.swift
+++ michisirube/michisirube/Extension/UIView+Extension.swift
@@ -57,4 +57,12 @@ extension UIView {
             self.layer.borderColor = newValue?.cgColor
         }
     }
+    
+    func addBorderBottom(height: CGFloat, color: UIColor) {
+        let border = CALayer()
+        border.frame = CGRect(x: 0, y: self.frame.height - height, width: self.frame.width, height: height)
+        border.backgroundColor = color.cgColor
+        self.layer.addSublayer(border)
+    }
+    
 }
