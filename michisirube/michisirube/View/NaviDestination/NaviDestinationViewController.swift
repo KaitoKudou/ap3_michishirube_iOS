9e14c4de1788350195fa49d2ab37da34937516c1
AnboYukari <b1018129@gmail.com>
2020-10-05 00:52:11 +0900

    [Add] 案内開始ボタンをタップ後，Google Mapsに画面遷移し，目的地確認画面で表示されていた場所のルート表示をした

--- michisirube/michisirube/View/NaviDestination/NaviDestinationViewController.swift
+++ michisirube/michisirube/View/NaviDestination/NaviDestinationViewController.swift
@@ -6,20 +6,26 @@
 //
 
 import UIKit
+import MapKit
 
 class NaviDestinationViewController: UIViewController {
 
     @IBOutlet weak var destinationView: UIView!
+    //緯度経度
+    let latitude = "41.796771"
+    let longitude = "140.757028"
+    var urlString: String! = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        //背景設定
         self.view.setBackground(name: "Background")
         navigationController?.setNavigationBarHidden(false, animated: true)
         
         // NavigationBarを透明化
-               navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
-               navigationController?.navigationBar.shadowImage = UIImage()
+        navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        navigationController?.navigationBar.shadowImage = UIImage()
                
     }
     
@@ -33,8 +39,18 @@ class NaviDestinationViewController: UIViewController {
         let storyboard = UIStoryboard(name: "NaviNavigating", bundle: nil)
         let naviNavigatingViewController = storyboard.instantiateViewController(identifier: "NaviNavigatingViewController") as NaviNavigatingViewController
         self.navigationController?.pushViewController(naviNavigatingViewController, animated: true)
+        
+        //Googleマップに遷移，もしGoogleマップのアプリがインストールされていなければ既存のマップアプリを起動する
+        if UIApplication.shared.canOpenURL(URL(string:"comgooglemaps://")!) {
+            urlString = "comgooglemaps://?daddr=\(latitude),\(longitude)&directionsmode=walking&zoom=14"
+            }
+        else {
+            urlString = "http://maps.apple.com/?daddr=\(latitude),\(longitude)&dirflg=w"
+            }
+        if let url = URL(string: urlString) {
+            UIApplication.shared.open(url)
+            }
+        UIApplication.shared.canOpenURL(URL(string:"comgooglemaps://")!)
     }
-    
-
     
 }
