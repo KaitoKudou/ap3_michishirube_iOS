fbf150b037ae1383289a261fecf539a81532a028
KaitoKudou <kaitokudou2468110921@gmail.com>
2020-10-12 19:58:42 +0900

    [Add]Apollo-iOSを導入

--- michisirube/michisirube/Model/Network.swift
+++ michisirube/michisirube/Model/Network.swift
@@ -5,4 +5,10 @@
 //  Created by 工藤海斗 on 2020/10/12.
 //
 
+import Apollo
 import Foundation
+
+class Network {
+    static let shared = Network()
+    private(set) lazy var apollo = ApolloClient(url: URL(string: "https://miraikeitai2020-bff.herokuapp.com/query")!)
+}
