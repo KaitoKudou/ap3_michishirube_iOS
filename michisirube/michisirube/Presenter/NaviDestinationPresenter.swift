37fb88ddbd81b3c448f05fb9df00087449a354ac
KaitoKudou <kaitokudou2468110921@gmail.com>
2020-10-13 21:01:11 +0900

    [Add]端末に保存した感情の値をリクエストの引数にした

--- michisirube/michisirube/Presenter/NaviDestinationPresenter.swift
+++ michisirube/michisirube/Presenter/NaviDestinationPresenter.swift
@@ -8,10 +8,13 @@
 import Foundation
 
 class NaviDestinationPresenter {
+    let userDefaults = UserDefaults.standard
+    let settingKey = "value" // UserDafaultを使う時のキー
     // サーバーから目的地のリストを受け取る
     func getDestinationList(completion: @escaping ([SpotsQuery.Data.Spot]) -> Void) {
-        
-        Network.shared.apollo.fetch(query: SpotsQuery(deviceLatitude: 41.796771, deviceLongitude: 140.757028, worktime: 0, emotion: 0)) { [weak self] result in
+        let emotionTypeNumber = userDefaults.integer(forKey: settingKey)
+        print("感情の種類の番号：\(emotionTypeNumber)")
+        Network.shared.apollo.fetch(query: SpotsQuery(deviceLatitude: 41.796771, deviceLongitude: 140.757028, worktime: 0, emotion: emotionTypeNumber)) { [weak self] result in
 
             guard self != nil else {
               return
