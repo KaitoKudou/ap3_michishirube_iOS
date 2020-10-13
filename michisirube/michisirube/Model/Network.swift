69f8cee580a167a7fa14490b7102735d9593aff6
KaitoKudou <kaitokudou2468110921@gmail.com>
2020-10-13 15:59:03 +0900

    [Add]トークンを付与してリクエストを飛ばすコードを書いた

--- michisirube/michisirube/Model/Network.swift
+++ michisirube/michisirube/Model/Network.swift
@@ -7,8 +7,20 @@
 
 import Apollo
 import Foundation
+import KeychainSwift
 
 class Network {
     static let shared = Network()
-    private(set) lazy var apollo = ApolloClient(url: URL(string: "https://miraikeitai2020-bff.herokuapp.com/query")!)
+        private(set) lazy var apollo: ApolloClient = {
+        let configuration = URLSessionConfiguration.default
+        configuration.httpAdditionalHeaders = ["id": "hoge", "pass": "pass"]
+        let client = URLSessionClient(sessionConfiguration: configuration, callbackQueue: .none)
+    
+        let endpointURL = URL(string: "https://miraikeitai2020-bff.herokuapp.com/query")!
+        let transport = HTTPNetworkTransport(url: endpointURL,
+        client: client)
+        
+        return ApolloClient(networkTransport: transport)
+    }()
+    
 }
--- michisirube/michisirube/Model/RequestLoggingInterceptor.swift
+++
@@ -1,8 +0,0 @@
-//
-//  RequestLoggingInterceptor.swift
-//  michisirube
-//
-//  Created by 工藤海斗 on 2020/10/13.
-//
-
-import Foundation
