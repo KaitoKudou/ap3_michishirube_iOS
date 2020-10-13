0630af34e00b4b3b8b146d6b16b341971b185632
KaitoKudou <kaitokudou2468110921@gmail.com>
2020-10-13 19:47:20 +0900

    [Add]サーバに目的地のリストを受け取るリクエストを送る処理を書いた

--- michisirube/michisirube/Presenter/NaviDestinationPresenter.swift
+++ michisirube/michisirube/Presenter/NaviDestinationPresenter.swift
@@ -8,9 +8,22 @@
 import Foundation
 
 class NaviDestinationPresenter {
-    
     // サーバーから目的地のリストを受け取る
-    func getDestinationList() {
+    func getDestinationList(completion: @escaping ([SpotsQuery.Data.Spot]) -> Void) {
         
+        Network.shared.apollo.fetch(query: SpotsQuery(deviceLatitude: 41.796771, deviceLongitude: 140.757028, worktime: 0, emotion: 0)) { [weak self] result in
+
+            guard self != nil else {
+              return
+            }
+
+            switch result {
+            case .success(let graphQLResult):
+                completion([graphQLResult.data!.spots])
+
+            case .failure(let error):
+                print("Failure! Error: \(error)")
+            }
+        }
     }
 }
