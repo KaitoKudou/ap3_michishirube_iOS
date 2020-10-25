955f8fbcc6fceff2dab566ec999d4760038f56ad
KaitoKudou <kaitokudou2468110921@gmail.com>
2020-10-26 04:54:53 +0900

    [Add]目的地を評価する部分のコードを書いた

--- michisirube/michisirube/Presenter/NaviEvaluationPresenter.swift
+++ michisirube/michisirube/Presenter/NaviEvaluationPresenter.swift
@@ -8,9 +8,33 @@
 import Foundation
 
 class NaviEvaluationPresenter {
+    let userDefaults = UserDefaults.standard
+    let settingKey = "value" // UserDafaultを使う時のキー(感情の値)
+    let settingSpotidKey = "spotid" // UserDafaultを使う時のキー(spotid)
+    let settingEvaluationStatusKey = "status" // UserDafaultを使う時のキー(評価のstatus)
     
     // 目的地の評価結果をサーバに送る
     func sendEvaluationResult() {
+        let emotionTypeNumber = userDefaults.integer(forKey: settingKey)
+        let spotid = userDefaults.string(forKey: settingSpotidKey)
+        let evaluationStatus = userDefaults.bool(forKey: settingEvaluationStatusKey)
+        print("感情の種類の番号：\(emotionTypeNumber)")
+        print("スポットID：\(String(describing: spotid))")
+        print("評価のステータス：\(evaluationStatus)")
         
+        Network.shared.apollo.perform(mutation: AddEvaluationMutation(spotid: spotid ?? "", emotion: emotionTypeNumber, status: evaluationStatus)) { [weak self] result in
+            
+            guard self != nil else {
+              return
+            }
+
+            switch result {
+            case .success(let graphQLResult):
+                print("success! : \([graphQLResult.data?.addEvaluation])")
+
+            case .failure(let error):
+                print("Failure! Error: \(error)")
+            }
+        }
     }
 }
