4b2ce99ddb31d836f6d34d4dc277123cf66ea060
KaitoKudou <kaitokudou2468110921@gmail.com>
2020-11-03 22:32:06 +0900

    [Add]寄り道についた後に通知の遷移先となる画面を作成

--- michisirube/michisirube/View/DetourInfomation/DetourInfomationViewController.swift
+++ michisirube/michisirube/View/DetourInfomation/DetourInfomationViewController.swift
@@ -12,18 +12,11 @@ class DetourInfomationViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.view.setBackground(name: "Background")
+        navigationController?.setNavigationBarHidden(false, animated: true)
+        
+        // NavigationBarを透明化
+        navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        navigationController?.navigationBar.shadowImage = UIImage()
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
