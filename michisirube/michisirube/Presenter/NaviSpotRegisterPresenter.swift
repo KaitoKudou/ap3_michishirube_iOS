637ae64caf72d43101ff631784d315533e4ac179
KaitoKudou <kaitokudou2468110921@gmail.com>
2020-11-12 00:15:01 +0900

    [Fix]場所名、説明、感情の種類をサーバに保存するためにpresenterで受け取る

--- michisirube/michisirube/Presenter/NaviSpotRegisterPresenter.swift
+++ michisirube/michisirube/Presenter/NaviSpotRegisterPresenter.swift
@@ -8,14 +8,24 @@
 import Foundation
 
 class NaviSpotRegisterPresenter {
-    var  base64SendProtocol: Base64SendProtocol!
+    var base64SendProtocol: Base64SendProtocol!
+    var imageData: String!
+    var placeText: String!
+    var explainText: String!
+    var emotionSellectNumber: Int!
+    let userDefaults = UserDefaults.standard
+    let settingEmotionSelectNumberKey = "emotionSellect" // UserDafaultを使う時のキー(感情の種類)
+    let settingPlaceTextKey = "placeText" // UserDafaultを使う時のキー(場所の名前)
+    let settingExplainTextKey = "explainText" // UserDafaultを使う時のキー(場所の詳細)
     
-    init(view: Base64SendProtocol) {
-        self.base64SendProtocol = view
+    init(base64SendProtocol: Base64SendProtocol) {
+        self.base64SendProtocol = base64SendProtocol
     }
     
-    func showBase64() {
-        let data = base64SendProtocol.sendBase64()
-        print(data)
+    func show() {
+        imageData = base64SendProtocol.sendBase64()
+        placeText = userDefaults.string(forKey: settingPlaceTextKey)
+        emotionSellectNumber = userDefaults.integer(forKey: settingEmotionSelectNumberKey)
+        explainText = userDefaults.string(forKey: settingExplainTextKey)
     }
 }
