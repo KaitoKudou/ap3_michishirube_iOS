cf14f3863b4ca6e5fb0af611be52299dd1cc0205
KaitoKudou <kaitokudou2468110921@gmail.com>
2020-11-17 22:44:23 +0900

    [Add]端末の緯度経度を引数にしてサーバに送った

--- michisirube/michisirube/Presenter/NaviDestinationPresenter.swift
+++ michisirube/michisirube/Presenter/NaviDestinationPresenter.swift
@@ -11,14 +11,18 @@ class NaviDestinationPresenter {
     let userDefaults = UserDefaults.standard
     let settingKey = "value" // UserDafaultを使う時のキー(感情の値)
     let settingWorkTimeKey = "time" // UserDafaultを使う時のキー(所要時間)
+    let settingDeviceLatitudeKey = "deviceLatitude" // デバイスの緯度
+    let settingDeviceLongitudeKey = "deviceLongitude" // デバイスの経度
     
     // サーバーから目的地のリストを受け取る
     func getDestinationList(completion: @escaping ([SpotsQuery.Data.Spot.Spot?]) -> Void) {
         let emotionTypeNumber = userDefaults.integer(forKey: settingKey)
         let workTime = userDefaults.integer(forKey: settingWorkTimeKey)
+        let latitude = userDefaults.double(forKey: settingDeviceLatitudeKey)
+        let longitude = userDefaults.double(forKey: settingDeviceLongitudeKey)
         print("感情の種類の番号：\(emotionTypeNumber)")
         print("所要時間：\(workTime)分")
-        Network.shared.apollo.fetch(query: SpotsQuery(deviceLatitude: 41.796771, deviceLongitude: 140.757028, worktime: workTime, emotion: emotionTypeNumber)) { [weak self] result in
+        Network.shared.apollo.fetch(query: SpotsQuery(deviceLatitude: latitude, deviceLongitude: longitude, worktime: workTime, emotion: emotionTypeNumber)) { [weak self] result in
 
             guard self != nil else {
               return
@@ -38,8 +42,10 @@ class NaviDestinationPresenter {
     // サーバから寄り道をもらう
     func getDetourList(completion: @escaping ([[SpotsQuery.Data.Spot.Detour?]?]) -> Void) {
         let emotionTypeNumber = userDefaults.integer(forKey: settingKey)
+        let latitude = userDefaults.double(forKey: settingDeviceLatitudeKey)
+        let longitude = userDefaults.double(forKey: settingDeviceLongitudeKey)
         print("感情の種類の番号：\(emotionTypeNumber)")
-        Network.shared.apollo.fetch(query: SpotsQuery(deviceLatitude: 41.796771, deviceLongitude: 140.757028, worktime: 0, emotion: emotionTypeNumber)) { [weak self] result in
+        Network.shared.apollo.fetch(query: SpotsQuery(deviceLatitude: latitude, deviceLongitude: longitude, worktime: 0, emotion: emotionTypeNumber)) { [weak self] result in
 
             guard self != nil else {
               return
