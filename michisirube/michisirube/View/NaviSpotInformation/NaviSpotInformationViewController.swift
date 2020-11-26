bcd55dde0b06ab3b5ea95c75ca50d372fa887a76
AnboYukari <b1018129@gmail.com>
2020-11-27 00:47:56 +0900

    [Add] [Add]スポット登録情報画面の背景とテストボタンの追加

--- michisirube/michisirube/View/NaviSpotInformation/NaviSpotInformationViewController.swift
+++ michisirube/michisirube/View/NaviSpotInformation/NaviSpotInformationViewController.swift
@@ -12,18 +12,18 @@ class NaviSpotInformationViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.view.setBackground(name: "Background")
+        navigationController?.setNavigationBarHidden(false, animated: true)
+        
+        // NavigationBarを透明化
+        navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        navigationController?.navigationBar.shadowImage = UIImage()
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
+     @IBAction func evaluateButton(_ sender: Any) {
+     }
+    
 
 }
